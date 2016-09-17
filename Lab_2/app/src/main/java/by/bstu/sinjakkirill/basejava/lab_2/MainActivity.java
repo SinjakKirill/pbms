8d189e94bd6905804e3298043f5860a2e403b5da
SinjakKirill <sinjak.kirill@gmail.com>
2016-09-17 11:22:05 +0300

    lab_2

--- Lab_2/app/src/main/java/by/bstu/sinjakkirill/basejava/lab_2/MainActivity.java
+++ Lab_2/app/src/main/java/by/bstu/sinjakkirill/basejava/lab_2/MainActivity.java
@@ -1,9 +1,18 @@
 package by.bstu.sinjakkirill.basejava.lab_2;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +22,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    protected void inputData(View view){
-        EditText textName = (EditText) findViewById(R.id.editText);
-        textName.setText("Kirill");
+    protected void inputName(View v){
+        final String fileName = "Base_Lab02.txt";
+        EditText textName = (EditText) findViewById(R.id.name);
+        EditText textSurname = (EditText) findViewById(R.id.surname);
+        File f = new File(super.getFilesDir(), fileName);
+        if(ExistBase(fileName)){
+            try{
+                FileWriter fw = new FileWriter(f, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write(textName.getText() + "; " + textSurname.getText() + "; " + "\r\n");
+                bw.close();
+                Log.d("Log_2", "Данные записаны в файл " + fileName);
+            }
+            catch (IOException e){
+                Log.d("Log_2", "Не удалось открыть " + fileName);
+            }
+        }
+        else{
+            AlertDialog.Builder b = new AlertDialog.Builder(this);
+            b.setTitle("Создается файл " + fileName).setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.d("Log_02", "Создание файла " + fileName);
+                }
+            });
+            AlertDialog ab = b.create();
+            ab.show();
+            try{
+                f.createNewFile();
+                Log.d("Log_02", "Файл " + fileName + " создан");
+            }
+            catch (IOException e){
+                Log.d("Log_02", "Файл " + fileName + " не создан");
+            }
+        }
+    }
+
+    private boolean ExistBase(String fname){
+        boolean rc = false;
+        File f = new File(super.getFilesDir(), fname);
+        if(rc = f.exists())
+            Log.d("Log_02", "Файл " + fname + " существует");
+        else Log.d("Log_02", "Файл " + fname + " не найден");
+        return rc;
     }
 }
