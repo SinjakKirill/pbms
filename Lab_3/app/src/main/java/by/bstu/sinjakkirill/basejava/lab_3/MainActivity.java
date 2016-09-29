e8378e102d74eaa7d5e7e574178f30dbb9215e24
SinjakKirill <sinjak.kirill@gmail.com>
2016-09-29 22:27:14 +0300

    lab_3

--- Lab_3/app/src/main/java/by/bstu/sinjakkirill/basejava/lab_3/MainActivity.java
+++ Lab_3/app/src/main/java/by/bstu/sinjakkirill/basejava/lab_3/MainActivity.java
@@ -1,5 +1,7 @@
 package by.bstu.sinjakkirill.basejava.lab_3;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,51 +26,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveData(View view){
-        final String fileName = "Lab_03.txt";
         EditText inputKey = (EditText) findViewById(R.id.inputKeyeditText);
         EditText inputValue = (EditText) findViewById(R.id.inputValueeditText);
+        final String fileName = "Lab_03.txt";
         File f = new File(super.getFilesDir(), fileName);
         //f.delete();
-
-        if(!ExistBase(fileName)){
-            try{
-                f.createNewFile();
-                RandomAccessFile rf = new RandomAccessFile(f, "rw");
-                for (int i = 0; i < 180; i++) {
-                    rf.writeChar('0');
+        if(inputKey.getText().length() == 5 && inputValue.getText().length() == 10){
+            if(!ExistBase(fileName)){
+                try{
+                    //создание файла, если он не существует
+                    f.createNewFile();
+                    RandomAccessFile rf = new RandomAccessFile(f, "rw");
+                    for (int i = 0; i < 180; i++) {
+                        rf.writeChar('0');
+                    }
+                    rf.close();
+                    writeData(inputKey.getText().toString(), inputValue.getText().toString(), f);
+                    Log.d("Log_03", "Файл " + fileName + " успешно создан");
                 }
-                rf.close();
-                writeData("sonya", "1234567890", f);
-                Log.d("Log_03", "Файл " + fileName + " успешно создан");
-        }
-            catch (IOException e){
-                Log.d("Log_03", "Не удалось создать файл " + fileName);
+                catch (IOException e){
+                    Log.d("Log_03", "Не удалось создать файл " + fileName);
+                }
+            }
+            else{
+                   writeData(inputKey.getText().toString(), inputValue.getText().toString(), f);
+                   Log.d("Log_03", "Данные записаны в таблицу");
             }
         }
         else{
-            writeData("sonya", "1234567890", f);
-            Log.d("Log_03", "Данные записаны в таблицу");
+            AlertDialog.Builder b = new AlertDialog.Builder(this);
+            b.setTitle("Incorrekt data!\nKey(5 symbol) Value(10 symbol)").setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.d("Log_02", "Неверный формат введенных данных.");
+                }
+            });
+            AlertDialog ab = b.create();
+            ab.show();
         }
 
-
-        try {
-            try{
-                RandomAccessFile rf = new RandomAccessFile(f, "rw");
-                byte[] _text = new byte[(int)f.length()];
-                rf.read(_text);
-                String _textString = new String(_text, "UTF-16");
-                TextView text = (TextView) findViewById(R.id.TesttextView);
-                text.setText(_textString);
-            }
-            catch (IOException e){
-                Log.d("Log_03", "Ошибка считывания.");
-            }
+        //вывод таблицы в TextView
+        try{
+            RandomAccessFile rf = new RandomAccessFile(f, "rw");
+            byte[] _text = new byte[(int)f.length()];
+            rf.read(_text);
+            String _textString = new String(_text, "UTF-16");
+            TextView text = (TextView) findViewById(R.id.TesttextView);
+            text.setText(_textString);
         }
-        catch (Exception e){
-            Log.d("Lab_03", "Не удалось записать данные");
+        catch (IOException e){
+            Log.d("Log_03", "Ошибка считывания.");
         }
     }
 
+    //проверка существования файла
     private boolean ExistBase(String fname){
         boolean rc = false;
         File f = new File(super.getFilesDir(), fname);
@@ -78,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
         return rc;
     }
 
+    //вычисление хэш-кода
     int hashCode(String key){
         char[] arr = new char[key.length()];
         arr = key.toCharArray();
@@ -87,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
         return number % 10;
     }
 
+    //запись данных в таблицу
     boolean writeData(String key, String value, File file){
         try {
             RandomAccessFile raFile = new RandomAccessFile(file, "rw");
@@ -104,27 +117,42 @@ public class MainActivity extends AppCompatActivity {
             else{
                 int point = numberStr * 36;
                 boolean numb = true;
+                //boolean checkWrite = false;
                 while (numb){
-                    int twoPoint;
-                    raFile.seek(point + 30);
-                    byte[] pointByte = new byte[6];
-                    raFile.read(pointByte);
-                    twoPoint = Integer.valueOf(new String(pointByte, "utf-16"));
-                    if(twoPoint == 0){
-                        raFile.seek(point + 30);
-                        raFile.writeChars(Integer.toString((int)raFile.length()));
-
-                        EditText q = (EditText) findViewById(R.id.inputKeyeditText);
-                        q.setText(Integer.toString(point));
-                        Log.d("Log_03", "Значение последнего указателя в списке: " + point);
+                    //проверка на совпадение ключей
+                    byte[] checkKey = new byte[10];
+                    raFile.seek(point);
+                    raFile.read(checkKey);
+                    String qwe = new String(checkKey, "utf-16");
+                    if(qwe.equals(key)){
+                        raFile.seek(point + 10);
+                        raFile.writeChars(value);
+                        //checkWrite = true;
                         numb = false;
+                        break;
                     }
-                    else{
-                        point = twoPoint;
+                        int twoPoint;
+                        raFile.seek(point + 30);
+                        byte[] pointByte = new byte[6];
+                        raFile.read(pointByte);
+                        twoPoint = Integer.valueOf(new String(pointByte, "utf-16"));
+                        if (twoPoint == 0) {
+                            raFile.seek(point + 30);
+                            raFile.writeChars(Integer.toString((int) raFile.length()));
+                            raFile.seek((int)raFile.length());
+                            raFile.writeChars(key + value + "000");
+                            Log.d("Log_03", "Значение последнего указателя в списке: " + point);
+                            numb = false;
+                        } else {
+                            point = twoPoint;
+                        }
                     }
-                }
-                raFile.seek((int)raFile.length());
-                raFile.writeChars(key + value + "000");
+                /*if(!checkWrite){
+                    raFile.seek((int)raFile.length());
+                    raFile.writeChars(key + value + "000");
+                }*/
+                //raFile.seek((int)raFile.length());
+                //raFile.writeChars(key + value + "000");
             }
             raFile.close();
             Log.d("Log_03", "Данные успешно записаны.");
@@ -136,7 +164,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    boolean readData(String key, File file){
+    boolean readData(String key, File file) {
         return true;
     }
 
